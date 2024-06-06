0ad56076781b009eff899e90d3721f3ad45bdc0e
Sasikumar <sasisachin12@yahoo.in>
2024-06-06 21:36:04 +0530

    check blue tooth enable conditions

--- app/src/main/java/aaa/android/bluetooth/MainActivity.kt
+++ app/src/main/java/aaa/android/bluetooth/MainActivity.kt
@@ -12,9 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import aaa.android.bluetooth.ui.theme.BlueToothConnectivityTheme
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,15 +33,31 @@ class MainActivity : ComponentActivity() {
         val bluetoothLEAvailable = packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)
 
 
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            val REQUEST_ENABLE_BT=1234
 
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address // MAC address
 
+            Log.e( "onCreate: ", "$deviceName $deviceHardwareAddress")
+        }
 
         enableEdgeToEdge()
         setContent {
             BlueToothConnectivityTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android"+bluetoothAvailable+bluetoothLEAvailable,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
